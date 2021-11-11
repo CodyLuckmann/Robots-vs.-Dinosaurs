3ad50c5afcf1618625615666138a99c4f3aaa677
CodyLuckmann <cluckmann@live.com>
2021-11-11 13:37:32 -0600

    Moved objects over from robot class and added them to the fleet list

--- fleet.py
+++ fleet.py
@@ -1,6 +1,15 @@
+from robots import Robots
+
 class Fleet:
     def __init__(self):
         self.robots = []
 
-    def create_fleet(self):
-        pass
+    def create_fleet(self, robot):
+        robot1 = Robots("R2D2")
+        robot2 = Robots("C3PO")
+        robot3 = Robots("BB8")
+        
+        self.robots.append(robot1)
+        self.robots.append(robot2)
+        self.robots.append(robot3)
+        
