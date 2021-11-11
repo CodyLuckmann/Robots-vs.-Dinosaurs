e2698a4aa5989911d46d3d722d19a643572154b6
CodyLuckmann <cluckmann@live.com>
2021-11-11 13:40:38 -0600

    Moved dino objects over from dinosaurs file, and added them to herd list

--- herd.py
+++ herd.py
@@ -1,6 +1,16 @@
+from dinosaurs import Dinosaurs
+
 class Herd:
     def __init__(self):
         self.dinosaurs = []
 
     def create_herd(self):
-        pass
+        dino1 = Dinosaurs("T-rex", 15)
+        dino2 = Dinosaurs("Raptor", 10)
+        dino3 = Dinosaurs("Tri-tops", 5)
+
+        self.dinosaurs.append(dino1)
+        self.dinosaurs.append(dino2)
+        self.dinosaurs.append(dino3)
+
+        
