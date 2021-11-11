d58d776d5694177924296d36e9216160b3e619ee
CodyLuckmann <cluckmann@live.com>
2021-11-11 11:21:11 -0600

    Added method to robots

--- robots.py
+++ robots.py
@@ -1,7 +1,11 @@
 from weapons import Weapon
+
+
 class Robots:
     def __init__(self, name):
         self.name = name
         self.health = 50
         weapon = Weapon("Sword", 5)
-        
+
+    def attack(self, dinosaur):
+        pass
